01705bdfb8fe2ab7a5ee2989d305368d34edfb53
Chris Sankat <csankat@gmail.com>
2020-11-17 14:04:19 -0600

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -2,7 +2,6 @@ songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS  users"
 song_table_drop = "DROP TABLE IF EXISTS  songs"
 artist_table_drop = "DROP TABLE IF EXISTS  artists"
-time_table_drop = "DROP TABLE IF EXISTS  time"
 
 # CREATE TABLES
 
@@ -38,14 +37,6 @@ artist_table_create = (""" CREATE TABLE IF NOT EXISTS artists(
                                , latitude float
                                , longitude float ) """)
 
-time_table_create = (""" CREATE TABLE IF NOT EXISTS time(
-                            start_time timestamp PRIMARY KEY
-                            , hour varchar
-                            , day varchar
-                            , week varchar
-                            , month varchar
-                            , year int
-                            , weekday varchar) """)
 
 # INSERT RECORDS
 
@@ -82,10 +73,6 @@ artist_table_insert = (""" INSERT INTO artists (artist_id, name, location, latit
                             , longitude = EXCLUDED.longitude""")
 
 
-time_table_insert = (""" INSERT INTO time (start_time, hour, day, week, month, year, weekday)
-                            VALUES (%s,%s,%s,%s,%s,%s,%s)
-                            ON CONFLICT(start_time)
-                            DO NOTHING""")
 
 # FIND SONGS
 
@@ -99,5 +86,5 @@ song_select = ("""SELECT song_id, songs.artist_id
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop]
